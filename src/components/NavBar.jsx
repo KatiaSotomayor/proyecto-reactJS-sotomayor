e05d5af951f13aa793325bbdcc75c55310a7adaf
Katia Sotomayor <katiasotomayor042@gmail.com>
2026-05-11 20:58:36 -0300

    Añadido de navbar y css

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+const Navbar = ()=> {
+    return(
+        <nav class="nav-container">
+            <a href="">Inicio</a>
+            <a href="">Productos</a>
+            <a href="">Sobre nosotras</a>
+        </nav>
+    )
+}
+
+export default Navbar
